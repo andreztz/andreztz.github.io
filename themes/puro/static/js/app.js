f875f5cae114a3c5c40212caf0b99db70bc49c5e
andreztz <andreztz@gmail.com>
2019-03-11 01:16:40 -0300

    update gitignore

---
+++ themes/puro/static/js/app.js
@@ -0,0 +1,12 @@
+var main = function() {
+    $("img").addClass("img-responsive");
+    $("div[class='youtube']").addClass('embed-responsive embed-responsive-16by9');
+    $('iframe').addClass("embed-responsive-item");
+    $("pre").css('border-radius', '0');
+    $(".btn-burguer").on('click', function() {
+        $(this).toggleClass("active");
+        $(".dropdown").toggleClass("dropdown-active");
+    });
+}
+
+$(document).ready(main);
