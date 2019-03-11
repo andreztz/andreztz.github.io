b26345cd7fec54c04d9cf44a95837a40fa37d873
andreztz <andreztz@gmail.com>
2019-03-11 00:03:25 -0300

    rm gem files

---
+++ venv/bin/rst2pseudoxml.py
@@ -0,0 +1,23 @@
+#!/mnt/51198660-c8a7-492a-bfb6-fdc21d433a8f/Projects/GitHub/andreztz.github.io/venv/bin/python
+
+# $Id: rst2pseudoxml.py 4564 2006-05-21 20:44:42Z wiemann $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing pseudo-XML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates pseudo-XML from standalone reStructuredText '
+               'sources (for testing purposes).  ' + default_description)
+
+publish_cmdline(description=description)
---
+++ venv/bin/rstpep2html.py
@@ -0,0 +1,25 @@
+#!/mnt/51198660-c8a7-492a-bfb6-fdc21d433a8f/Projects/GitHub/andreztz.github.io/venv/bin/python
+
+# $Id: rstpep2html.py 4564 2006-05-21 20:44:42Z wiemann $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing HTML from PEP
+(Python Enhancement Proposal) documents.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates (X)HTML from reStructuredText-format PEP files.  '
+               + default_description)
+
+publish_cmdline(reader_name='pep', writer_name='pep_html',
+                description=description)
